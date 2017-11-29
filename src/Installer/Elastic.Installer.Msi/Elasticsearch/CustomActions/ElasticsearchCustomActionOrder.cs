9db943e8aa1761c80849e8064be26a3128dabfea
Russ Cam <russ.cam@elastic.co>
2017-11-29 18:56:38 +1100

    Remove UninstallPluginsTask
    This task is no longer necessary in the upgrade flow, since the uninstallation of the old version happens after the installation of the new.

--- src/Installer/Elastic.Installer.Msi/Elasticsearch/CustomActions/ElasticsearchCustomActionOrder.cs
+++ src/Installer/Elastic.Installer.Msi/Elasticsearch/CustomActions/ElasticsearchCustomActionOrder.cs
@@ -8,22 +8,20 @@
 		ServiceParameters = 3,
 
 		// Deferred actions
-		SetPreconditions = 1,
-		InstallPreserveInstall = 2,
-		InstallDirectories = 3,
-		InstallConfiguration = 4,
-		InstallJvmOptions = 5,
-		InstallPlugins = 6,
-		BootstrapPassword = 7,
-		ServiceStartType = 8,
-		SetupXPackPasswords = 9,
+		InstallPreserveInstall = 1,
+		InstallDirectories = 2,
+		InstallConfiguration = 3,
+		InstallJvmOptions = 4,
+		InstallPlugins = 5,
+		BootstrapPassword = 6,
+		ServiceStartType = 7,
+		SetupXPackPasswords = 8,
 
 		// Rollback actions are played in reverse order
 		RollbackDirectories = 1,
 	
 		// Uninstall actions
-		UninstallPlugins = 1,
-		UninstallDirectories = 2,
+		UninstallDirectories = 1,
 
 		// Commit actons
 		CleanupInstall = 1,
